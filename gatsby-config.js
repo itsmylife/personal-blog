3bafdc109e0c0a6e66c19acf0730aada8265988b
ismail.simsek <ismail.simsek@gutefrage.net>
2019-09-12 21:47:20 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-7166316-11`,
       },
     },
     {
