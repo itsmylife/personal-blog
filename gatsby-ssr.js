49d278ff01cdcac0fadb59bf83fdb0b524e9c4be
ismail simsek <ismailsimsek09@gmail.com>
2020-04-16 09:54:33 +0200

    Add gatsby-ssr.js

---
+++ gatsby-ssr.js
@@ -0,0 +1,41 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+/* eslint-disable */
+const React = require('react');
+
+exports.onRenderBody = ({setHeadComponents, setPreBodyComponents}) => {
+  setHeadComponents([
+    // Google Tag Manager.
+    <script
+      defer
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KS28MZS');
+        `
+      }}
+    />,
+  ]);
+  setPreBodyComponents([
+    <noscript
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KS28MZS" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+      }}
+    />,
+  ]);
+};
+
+exports.wrapRootElement = ({element}) => {
+  return (
+    <Reporter.ErrorReporter>
+      {element}
+    </Reporter.ErrorReporter>
+  );
+}
